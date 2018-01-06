c3fb00f6c9f9ab038fa3d93c9a0cf24525a5cf45
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-01-05 22:44:14 -0600

    últimos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA  } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
+import { RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ROUTES } from './app.routing';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(ROUTES)
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
