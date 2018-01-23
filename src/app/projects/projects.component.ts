28e4b133742639937fbc4aa4f467cde9da3f7189
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-01-22 23:48:44 -0600

    Se añadió paginación al componente 'ProjectsComponent'

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Project } from '../models/Project';
 import { GithubUser } from '../models/GithubUser';
 import { ProjectsService } from './projects.service';
@@ -10,7 +10,7 @@ import { Member } from '../models/Member';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, AfterViewInit {
   readonly projects: Array<Project>
   readonly extractedMembers: Array<Member>
   constructor(public projectService: ProjectsService) { 
@@ -19,6 +19,18 @@ export class ProjectsComponent implements OnInit {
 
   ngOnInit() {
 
+  }
+
+  ngAfterViewInit() {
+    /**
+     * lo siguiente es necesario para quitar el padding por defecto que trae la libreria para paginació
+     */ 
+    let style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.zero-padding { padding: 0; background-color: #424242 }';
+    document.getElementsByTagName('head')[0].appendChild(style);
+    let ul = document.getElementsByClassName("ngx-pagination")
+    ul[0].classList.add('zero-padding')
   }
 
 }
